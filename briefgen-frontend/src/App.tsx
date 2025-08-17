91b44e5b32fbfe520a13ba18b036c33f4f90c44d
PrinceDisant <disantupadhyay07@gmail.com>
2025-08-17 16:43:57 +0530

    cleanup

--- briefgen-frontend/src/App.tsx
+++ briefgen-frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Wizard from './pages/Wizard'
 import TopNav from './components/TopNav'
-import AuthPage from "./pages/Auth";
 
 export default function App() {
   return (
@@ -10,10 +9,8 @@ export default function App() {
       <TopNav />
       <div className="mx-auto max-w-4xl p-4">
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/wizard" element={<Wizard />} />
           <Route path="/" element={<Navigate to="/wizard" replace />} />
-          <Route path="/auth" element={<AuthPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
